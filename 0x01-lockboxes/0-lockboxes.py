a8757ddfdc312072166985286684e976ec0f88e7
Anselme <anselme.s@hitech-services.com>
2023-01-05 19:29:09 +0000

    ALX INTERVIEW PROJECT

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+'''
+-You have n number of locked boxes in front of you. Each box is
+numbered sequentially from 0 to n - 1 and each box may contain
+keys to the other boxes.
+
+Write a method that determines if all the boxes can be opened.
+
+    Prototype: def canUnlockAll(boxes)
+    boxes is a list of lists
+    A key with the same number as a box opens that box
+    You can assume all keys will be positive integers
+        There can be keys that do not have boxes
+    The first box boxes[0] is unlocked
+    Return True if all boxes can be opened, else return False
+
+'''
+
+
+def canUnlockAll(boxes):
+    '''Checks if all boxes are unlockable'''
+    unlocked = [0]
+    locked = []
+    for i in range(len(boxes)):
+        if i in unlocked:
+            unlocked += boxes[i]
+        else:
+            locked.append(i)
+    locked = set(locked)
+    for j in set(locked):
+        if j in unlocked:
+            unlocked += boxes[j]
+            locked.remove(j)
+    return (len(locked) == 0)
