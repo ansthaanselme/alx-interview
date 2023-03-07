696f1437d9510b16880db9e728e6df2311d4decc
Anselme <anselme.s@hitech-services.com>
2023-03-07 08:51:42 +0000

    ALX INTERVIEW REVIEW

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,53 +1,42 @@
 #!/usr/bin/python3
-'''
-Maria and Ben are playing a game. Given a set of consecutive integers
-starting from 1 up to and including n, they take turns choosing a prime
-number from the set and removing that number and its multiples from
-the set. The player that cannot make a move loses the game.
-
-They play x rounds of the game, where n may be different for each round.
-Assuming Maria always goes first and both players play optimally,
-determine who the winner of each game is.
-
-- Prototype: def isWinner(x, nums)
-- where x is the number of rounds and nums is an array of n
-- Return: name of the player that won the most rounds
-- If the winner cannot be determined, return None
-- You can assume n and x will not be larger than 10000
-- You cannot import any packages in this task
-'''
+"""0. Prime Game - Maria and Ben are playing a game"""
 
 
 def isWinner(x, nums):
-    '''
-    Returns the name of the winner
-    '''
-    winner = []
-    for num in nums:
-        winner.append(roundWinner(x, num))
-    if winner.count('Ben') > (len(winner)/2):
-        return 'Ben'
-    return 'Maria'
+    """x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+
+    ben = 0
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
 
 
-def roundWinner(x, num):
-    '''
-    Calculates who wons every round
-    '''
-    if num == 1:
-        return 'Ben'
-    if num == 2:
-        return 'Maria'
-    prime_numbers = 1
-    for n in range(3, num + 1):
-        i = 1
-        not_prime = 0
-        while (n - i) >= 2:
-            if n % (n - i) == 0:
-                not_prime += 1
-            i += 1
-        if not_prime == 0:
-            prime_numbers += 1
-    if prime_numbers % 2 == 0:
-        return 'Ben'
-    return 'Maria'
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
